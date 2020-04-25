a36af54153895c74fc3caa989b38734cbef6bfc2
khorevnikita <46758480+khorevnikita@users.noreply.github.com>
2020-04-25 12:55:19 +0300

    Update bootstrap.php
    ++

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -5,13 +5,13 @@
  * Date: 07.08.19
  * Time: 10:19
  */
-include "core/global.php";
-require_once 'Core/querybuilder.php';
-require_once 'Core/model.php';
-require_once 'Core/view.php';
-require_once 'Core/controller.php';
+include "Core/global.php";
+require_once 'Core/QueryBuilder.php';
+require_once 'Core/Model.php';
+require_once 'Core/View.php';
+require_once 'Core/Controller.php';
 
 require_once 'WebRoutes.php';
-require_once 'Core/router.php';
+require_once 'Core/Router.php';
 
 \App\Router::start(); // запускаем маршрутизатор
